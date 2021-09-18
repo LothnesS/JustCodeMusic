0a4a85c272a284e4f0cbc208af111444accfbd12
Glitch (glitch-hello-node) <none>
2021-09-18 10:38:37 +0000

    ⛴💨 Checkpoint ./package.json:36225538/4366 ./hunter.js:36225538/120

--- hunter.js
+++ hunter.js
@@ -1,19 +1,15 @@
-var aoi = require('aoi.js')
+var ss = require('dbd.js')
 var fs = require('fs')
-var hunter = new aoi.Bot({
+var bot = new ss.Bot({
 	token:process.env.token,
 	prefix:"!",
 })
-hunter.onMessage()
+bot.onMessage()
 var reader = fs.readdirSync("./komutlar/").filter(file => file.endsWith(".js"))
 for(const file of reader) {	
 	const command = require(`./komutlar/${file}`)
-	hunter.command({
-		name: command.name,
-		code: command.code
-	})
-}
-hunter.status({
+  }
+bot.status({
   text: "OYNUYOR BÖLÜMÜ",
   type: "WATCHING",//ŞU AN İZLİYOR AYARLI  | PLAYING = OYNUYOR  | LISTENING  =  DİNLİYOR
   status: "dnd",//ŞU AN RAHATSIZ ETMEYİNDE  | onlıne | ıdle = Boşta 
