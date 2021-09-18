49c60e8e76a168042e95e455edf700d7f77ecfe3
Glitch (glitch-hello-node) <none>
2021-09-18 10:50:10 +0000

    😋🚇 Checkpoint ./hunter.js:36160604/48:36225538/519

--- hunter.js
+++ hunter.js
@@ -1,16 +1,26 @@
-var ss = require('dbd.js')
+var ss = require('aoi.js')
 var fs = require('fs')
 var bot = new ss.Bot({
 	token:process.env.token,
 	prefix:"!",
 })
-bot.onMessage()
-var reader = fs.readdirSync("./komutlar/").filter(file => file.endsWith(".js"))
-for(const file of reader) {	
-	const command = require(`./komutlar/${file}`)
-  }
+bot.onLeave();
+bot.onJoined();
+bot.onMessage();
+var reader = fs.readdirSync('./komutlar').filter(file => file.endsWith('.js'));
+for (const file of reader) {
+	const command = require(`./komutlar/${file}`);
+	bot.command({
+		name: command.name,
+		aliases: command.aliases,
+		bkz: command.bkz,
+		code: command.code
+	});
+
+
+}
 bot.status({
-  text: "OYNUYOR BÖLÜMÜ",
+  text: "YouTube / Kadir Mercan",
   type: "WATCHING",//ŞU AN İZLİYOR AYARLI  | PLAYING = OYNUYOR  | LISTENING  =  DİNLİYOR
   status: "dnd",//ŞU AN RAHATSIZ ETMEYİNDE  | onlıne | ıdle = Boşta 
   time: 12
